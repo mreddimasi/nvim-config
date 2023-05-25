ccd9f80b37b36f45b1b61c498920781efab38d4e
mreddimasi <mreddimasi@gmail.com>
2023-05-25 18:38:24 +0530

    nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,44 @@
+return require('packer').startup(function(use)
+  -- Configurations will go here soon
+ use 'wbthomason/packer.nvim'
+ use 'williamboman/mason.nvim'   
+ use 'williamboman/mason-lspconfig.nvim'
+ use 'neovim/nvim-lspconfig'
+ use 'hrsh7th/nvim-cmp' 
+ use 'hrsh7th/cmp-nvim-lsp'
+ use 'hrsh7th/cmp-nvim-lua'
+ use 'hrsh7th/cmp-nvim-lsp-signature-help'
+ use 'hrsh7th/cmp-vsnip'                             
+ use 'hrsh7th/cmp-path'                              
+ use 'hrsh7th/cmp-buffer'                            
+ use 'hrsh7th/vim-vsnip'
+-- DAP for debugging
+ use 'mfussenegger/nvim-dap'
+ -- UI for DAP
+ use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+-- File explorer tree
+ use {
+  'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+ }
+ -- Dracula theme for styling
+ use 'Mofiqul/dracula.nvim'
+  -- Treesitter
+ use {
+  -- recommended packer way of installing it is to run this function, copied from documentation
+         'nvim-treesitter/nvim-treesitter',
+      run = function()
+              local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+              ts_update()
+         end,
+
+ }
+
+ -- Lualine information / Status bar
+ use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+ }
+end)

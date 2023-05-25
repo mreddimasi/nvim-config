a1cf43fa59cec68d08bbd3d71f0991a1863baef7
mreddimasi <mreddimasi@gmail.com>
2023-05-25 19:07:44 +0530

    Telescope added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,4 +41,14 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
  }
+
+ -- Telescope used to fuzzy search files
+ use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+ }
+
+
+
+
 end)
